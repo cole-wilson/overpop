cf7625f112794e19d750764e5eacc16f346b8989
Cole Wilson <cole@colewilson.xyz>
2021-01-03 22:58:02 +0000

    a

--- info.js
+++ info.js
@@ -22,14 +22,24 @@ The following interactive slides will go more in depth into the subject of Earth
 
 <content>
 
+Humans are involved in overpoplation because we are mainly the ones causing it: there are about 7.6 Billion of us! (As of 2021.) This is be
+
 There are many human impacts that cause overpopulation. These include some of the following:
 
- - One human impact is the decrease of the death rate. The decrease of this rate means that less children are dying, so more of them will grow up to have children of their own.  
+ - One human impact is the decrease of the death rate. The decrease of this rate means that less children are dying, so more of them will grow up to have children of their own.<br>While it's good less people are dying, this will drastically increase the overall population, which isn't good.<br>![Graph of infant mortality rate](https://ourworldindata.org/exports/child-mortality-by-income-level-of-country_v2_850x600.svg)<br><caption>Infant mortality rate from [Our World In Data](https://ourworldindata.org/child-mortality)</caption>
+
+ - Another human impact
+
+
 
 
 </content>
 
 `,
+
+// SOURCES
+
+
 `
 # Sources
 
@@ -37,9 +47,14 @@ There are many human impacts that cause overpopulation. These include some of th
 
 ## Works Cited:
 
+(slide images are sourced with captions, planets from Google Images and ezgif)
+
 <pre>
 
-bibliography here
+N/A, Rinkesh. “Overpopulation: Causes, Effects and Solutions.” Conserve Energy Future, 2020, 		
+	https://www.conserve-energy-future.com/causes-effects-solutions-of-overpopulation.php. 
+	Accessed 3 Janurary 2021.
+
 
 </pre>
 
